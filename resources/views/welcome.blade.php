82b979a9fb9ed1101fd8daebc0acc6a2317b1b23
ryften <ezequieldavid.n@gmail.com>
2021-01-15 17:25:17 -0300

    added image to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,10 @@
         <div class="row justify-content-start">
             <div class="col-md-6  mt-5">
                 <div class="col-md-12">
-                    <p class="text-xl-center" style="font-size: 1.6em">Welcome to <strong>Summas-rest</strong> system, where the employee
+                    <p class="text-xl-center" style="font-size: 1.6em">Welcome to <strong class="text-primary">Summas-rest</strong> system, where the employee
                         management is how take a walk in the park
                     </p>
+                    <img src="{{asset('assets/developer.jpg')}}" style="width: 100%; height: 55vh" alt="">
                 </div>
             </div>
 
